f124ec387dc482e781d02dde7e12b30d562b6eb8
Santosh K Shetty <65031498+SantoshKShettyApps@users.noreply.github.com>
2024-04-08 19:47:10 +0530

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,9 +1,16 @@
-const Service = require("../../lib/service");
+import dlv from "dlv";
+
+const Obj = {
+  test: {
+    best: {
+      profile: "sjadlfjdskljf"
+    }
+  }
+};
 
 export default () => {
   const ENV = Netlify.env.get("READ_THIS_ENV");
-  const response = Service();
-  return new Response(`${response} : ${ENV}`);
+  return new Response(`${dlv(Obj, "test.best.profile")} : ${ENV}`);
 }
 
 export const config = { path: "/test" };
